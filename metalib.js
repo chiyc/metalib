994878320f0d0bae78053575d2871bf2e40725a6
chi <cychoi@zoho.com>
2021-03-23 19:56:45 -0400

    Add script for meta title update test

---
+++ metalib.js
@@ -0,0 +1,10 @@
+if (document.location.href === 'https://www.taggingguru.com/my-team') {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function() {
+      document.title = 'Tagging Guru | Contributors | Tagging Guru Team Assemble';
+      console.log('updated title');
+    })
+    .catch(function(err) {
+      console.error('errror updating title', err);
+    });
+}
