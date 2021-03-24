72be3af06c6a71f94db7c3648b6ce30a0c6b5fee
chi <cychoi@zoho.com>
2021-03-23 20:19:23 -0400

    Change meta title name to detect change

--- metalib.js
+++ metalib.js
@@ -1,7 +1,7 @@
 if (document.location.href === 'https://www.taggingguru.com/my-team') {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(function() {
-      document.title = 'Tagging Guru | Contributors | Tagging Guru Team Assemble';
+      document.title = 'Tagging Guru | Contributors | Tagging Guru Team Unite';
       console.log('updated title');
     })
     .catch(function(err) {
