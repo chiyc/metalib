e8402d28e97d95612339f104b1f425fd7342e0b0
Chi <cychoi@zoho.com>
2021-03-29 18:11:05 -0400

    Tweak console log message

--- metalib.js
+++ metalib.js
@@ -2,7 +2,7 @@ if (document.location.href === 'https://www.taggingguru.com/my-team') {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(function() {
       document.title = 'Tagging Guru | Contributors | Tagging Guru Team Unite';
-      console.log('updated title');
+      console.log('Updated title!');
     })
     .catch(function(err) {
       console.error('errror updating title', err);
